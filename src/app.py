4deeef32140a036ace664b73a5df2641c5bf1a28
auri <auriromero@gmail.com>
2024-04-29 19:44:07 +0200

    update app.py probar linter.yml en GH

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 
+
 # From https://github.com/datademofun/heroku-basic-flask
 # Nota: en ese tutorial se ve como hacerlo 
 #  - con Heroku CLI
 #  - sin usar GitHub
 #   Aunque luego al final lo manda a GitHub
+
 
 from flask import Flask, render_template
 from datetime import datetime
@@ -19,15 +21,15 @@ def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
 
     # Render HTML with variable
-    return render_template("index.html", the_time=the_time, tema="cats" ) 
+    return render_template("index.html", the_time=the_time, tema="cats" )
 
     # return f"""
     # <h1>Hello heroku</h1>
     # <p>It is currently {the_time}.</p>
     # <img src="http://loremflickr.com/600/400" />
     # """
-
-
+   
+   
 @app.route('/spain')
 def homepage_spain():
 
@@ -39,3 +41,4 @@ def homepage_spain():
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True) 
+
