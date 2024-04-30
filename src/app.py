61211a8aea2a403cb0a78187d624c4c892053585
auri <auriromero@gmail.com>
2024-04-30 18:07:23 +0200

    reviado linting en app.py

--- src/app.py
+++ src/app.py
@@ -1,16 +1,12 @@
-
-
 # From https://github.com/datademofun/heroku-basic-flask
 # Nota: en ese tutorial se ve como hacerlo 
 #  - con Heroku CLI
 #  - sin usar GitHub
 #   Aunque luego al final lo manda a GitHub
 
-
 from flask import Flask, render_template
 from datetime import datetime
 import locale
-
 
 app = Flask(__name__)
 locale.setlocale(locale.LC_TIME, '')
@@ -28,9 +24,8 @@ def homepage():
     # <p>It is currently {the_time}.</p>
     # <img src="http://loremflickr.com/600/400" />
     # """
-   
-   
-@app.route('/spain') 
+
+@app.route('/spain')
 def homepage_spain():
 
     the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
@@ -41,4 +36,3 @@ def homepage_spain():
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True) 
-
